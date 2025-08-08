4c6f8abc5f827ec9309ec9e03d0d6fde4dfd8cfe
Lucas Monteiro de Oliveira <lucas.monteiro280898@gmail.com>
2025-08-08 15:51:54 -0300

    Update PatientRepository.java

--- src/main/java/io/github/lucasoliveira28/medicalappointmentapi/repository/PatientRepository.java
+++ src/main/java/io/github/lucasoliveira28/medicalappointmentapi/repository/PatientRepository.java
@@ -4,7 +4,16 @@ import io.github.lucasoliveira28.medicalappointmentapi.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
     public Patient findPatientById(Long id);
+
     public Patient findPatientByName(String name);
+
     public Patient findPatientByCpf(String cpf);
+
+    public Boolean existsByEmail(String email);
+
+    public Boolean existsByPhone(String phone);
+
+    public Boolean existsByCpf(String cpf);
 }
